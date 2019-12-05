ef6387642aa51c8aa84718f39d5ea09f6f73be12
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-05 05:19:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,7 +54,7 @@ def reduce_to_all_true(source_array)
   while index < source_array.length do
     if source_array[index] == false 
       return false
-      elsif source_array[index] == true
+      && source_array[index] == true
       return true 
     end 
     index += 1 
