65a507f3d357226b0e9699c4abbf760c595ef35f
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-05 05:28:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,6 +41,7 @@ end
 
 def reduce_to_total(source_array, starting_point)
   index = 0 
+  starting_point = 0 
   total = starting_point
   while index < source_array.length do
     total += source_array[index]
